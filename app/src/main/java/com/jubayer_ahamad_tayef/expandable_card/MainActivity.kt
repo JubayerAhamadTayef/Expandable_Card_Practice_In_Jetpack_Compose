e27b178b2c635eb311ec7f907de4059bb14d430d
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-24 18:33:55 +0600

    Create a new empty compose android project for practice extandable card!

---
+++ app/src/main/java/com/jubayer_ahamad_tayef/expandable_card/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jubayer_ahamad_tayef.expandable_card
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jubayer_ahamad_tayef.expandable_card.ui.theme.Expandable_CardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Expandable_CardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Expandable_CardTheme {
+        Greeting("Android")
+    }
+}
